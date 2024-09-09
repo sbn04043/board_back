308ec7b2875bdec0cda0ae80da59f3dad3e4494f
sbn04043 <70567694+sbn04043@users.noreply.github.com>
2024-09-09 16:04:43 +0900

    user pagination

---
+++ src/main/java/live/greenmarket/board_back/pattern/proxy/Pagination.java
@@ -0,0 +1,9 @@
+package live.greenmarket.board_back.pattern.proxy;
+
+import lombok.Data;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component("page") @Data @Lazy
+public class Pagenation {
+}
